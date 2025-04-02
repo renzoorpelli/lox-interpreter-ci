1081387a47e2a42419e10f15d805c1a47b8cba6a
renzo.orpelli <orpellirenzo@gmail.com>
2025-04-02 20:27:39 -0300

    scan_token started

--- src/Scanner.rs
+++ src/Scanner.rs
@@ -1,32 +1,27 @@
 use crate::Token::{Token, TokenKind};
+use std::process::exit;
 
 pub struct Scanner {
     source: String,
     tokens: Vec<Token>,
-    start: usize,   // start of the current lexeme
-    current: usize, // current char position
-    line: usize,    // current line
+    start: usize,   // points to the first character of the lexeme => offset
+    current: usize, // points at the character currently being considered => offset
+    line: usize,    // track what source line current is on.
     column: usize,  // current column
 }
 
 impl Scanner {
-    pub fn new(
-        source: String,
-        tokens: Vec<Token>,
-        start: usize,
-        current: usize,
-        line: usize,
-        column: usize,
-    ) -> Self {
+    pub fn new(source: String, tokens: Vec<Token>, column: usize) -> Self {
         Scanner {
             source,
             tokens,
-            start,
-            current,
-            line,
+            start: 0,
+            current: 0,
+            line: 1,
             column,
         }
     }
+    /// method used to check all the characters were consumed
     fn is_at_the_end(&self) -> bool {
         self.current >= self.source.len()
     }
@@ -40,7 +35,7 @@ impl Scanner {
 
         // push EOF token to the vector
         self.tokens.push(Token::new(
-            String::from("EOF"),
+            String::from(""),
             TokenKind::Eof,
             self.line,
             self.column,
@@ -51,25 +46,92 @@ impl Scanner {
 
     fn add_token(&mut self, kind: TokenKind) {
         let value = &self.source[self.start..self.current].to_string();
-        self.tokens.push(Token::new(
-            value.to_string(),
-            kind,
-            self.line,
-            self.column,
-        ));
+        self.tokens
+            .push(Token::new(value.to_string(), kind, self.line, self.column));
     }
-    fn scan_token(&mut self) {}
+    fn scan_token(&mut self) {
+        match self.move_next() {
+            '+' => self.add_token(TokenKind::Plus),
+            '-' => self.add_token(TokenKind::Minus),
+            '*' => self.add_token(TokenKind::Star),
+            '/' => match self.peek_match('/') {
+                true => {
+                    //  A comment goes until the end of the line
+                    while self.peek() != '\n' && !self.is_at_the_end() {
+                        self.move_next();
+                    }
+                }
+                false => self.add_token(TokenKind::Slash),
+            },
+            '(' => self.add_token(TokenKind::LeftParen),
+            ')' => self.add_token(TokenKind::RightParen),
+            '{' => self.add_token(TokenKind::LeftBrace),
+            '}' => self.add_token(TokenKind::RightBrace),
+            ',' => self.add_token(TokenKind::Comma),
+            '.' => self.add_token(TokenKind::Dot),
+            '!' => match self.peek_match('=') {
+                true => self.add_token(TokenKind::BangEqual),
+                false => self.add_token(TokenKind::Bang),
+            },
+            '>' => match self.peek_match('=') {
+                true => self.add_token(TokenKind::GreaterEqual),
+                false => self.add_token(TokenKind::Greater),
+            },
+            '<' => match self.peek_match('=') {
+                true => self.add_token(TokenKind::LessEqual),
+                false => self.add_token(TokenKind::Less),
+            },
+            '=' => match self.peek_match('=') {
+                true => self.add_token(TokenKind::EqualEqual),
+                false => self.add_token(TokenKind::Equal),
+            },
+            _ => self.print_error("unexpected character"),
+        }
+    }
+
+    /// this method will consume the next character of the source by incrementing the position by one
+    fn move_next(&mut self) -> char {
+        let c = self.source[self.current..].chars().next().unwrap();
+        self.current += 1;
+        c
+    }
+
+    /// this method will peek the current char but NOT consume the token => Lookahead.
+    fn peek(&self) -> char {
+        if self.is_at_the_end() {
+            '\0'
+        }
+        self.source[self.current..].chars().next().unwrap()
+    }
+
+    /// this method will get the current character of the source
+    fn get_current(&self) -> char {
+        self.source.chars().nth(self.current).unwrap()
+    }
+
+    /// peek match will check if the given `char` is the same as the next one then return true and update the position, otherwise false
+    fn peek_match(&mut self, next: char) -> bool {
+        if self.is_at_the_end() || self.get_current() != next {
+            false;
+        }
+        self.current += 1;
+        true
+    }
+
+    /// helper method to generate a string with a comprehensive log error message for the user
     fn error_details(&self, message: &str) -> String {
         let line_content = self.source.lines().nth(self.line - 1).unwrap_or("");
-        let mut indicator = " ".repeat(self.column) + "^"; // Create a pointer to show the error pos
+        let indicator = " ".repeat(self.column) + "^"; // Create a pointer to show the error pos
 
         format!(
             "Error on line {}, column {}:\n{}\n{}\n{}",
             self.line, self.column, line_content, indicator, message
         )
     }
-    pub fn had_error(&self) -> Option<String> {
-        // or None
-        Some(self.error_details(&self.source))
+
+    /// this method will print an error and ends the process
+    fn print_error(&self, message: &str) {
+        eprintln!("{}", self.error_details(message));
+        exit(65)
     }
 }
